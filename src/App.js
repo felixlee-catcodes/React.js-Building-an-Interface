8dd4995a9146bafd624405f78aa0e9199444f914
C. Felix Lee <felixlee.music@gmail.com>
2023-05-23 19:51:22 -0400

    need to debug postcss & tailwind dependency

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { BiArchive } from "react-icons/bi"
+import { VscCalendar } from "react-icons/vsc";
 
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive />Your Appointments</h1>
+    <div className='App container mx-auto mt-3 font-thin'>
+      <h1 className='text-5xl'>
+        <VscCalendar />
+        Your Appointments
+      </h1>
     </div>
   );
 }
