21affd5b9d4ebb4812ac5c253e3a53ad9b613f3d
C. Felix Lee <felixlee.music@gmail.com>
2023-05-24 00:22:16 -0400

    installing tailwinf css in react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='App container mx-auto mt-3 font-thin'>
       <h1 className='text-5xl'>
-        <VscCalendar />
+        <VscCalendar className='inline-block text-yellow-400 align-top' />
         Your Appointments
       </h1>
     </div>
