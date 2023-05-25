7573ebaba22687dacaaa85117aacb31285ad2a0e
C. Felix Lee <felixlee.music@gmail.com>
2023-05-25 02:46:58 -0400

    useEffect and useCallback hooks for fetching data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import { BiCalendar } from "react-icons/bi"
-import Search from "./components/Search"
-import AddAppointment from "./components/AddAppointment"
-import AppointmentInfo from "./components/AppointmentInfo"
-import appointmentList from "./data.json"
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
+// import appointmentList from "./data.json";
+import { useState, useEffect, useCallback } from "react";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => setAppointmentList(data));
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
-    <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl mb-3">
-        <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
+    <div className='App container mx-auto mt-3 font-thin'>
+      <h1 className='text-5xl mb-3'>
+        <BiCalendar className='inline-block text-red-400 align-top' />
+        Your Appointments
+      </h1>
       <AddAppointment />
       <Search />
 
-      <ul className="divide-y divide-gray-200">
-        {appointmentList
-          .map(appointment => (
-            <AppointmentInfo key={appointment.id}
-              appointment={appointment}
-            />
-          ))
-        }
+      <ul className='divide-y divide-gray-200'>
+        {appointmentList.map((appointment) => (
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
+        ))}
       </ul>
     </div>
   );
